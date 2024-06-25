2f7803b4a9129287c724387815a6bb1599be3b0b
Akshit Garg <153659573+AkshitGarg24@users.noreply.github.com>
2024-06-25 21:17:51 +0530

    Made Website Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -47,40 +47,40 @@ function App() {
   }
 
   return (
-    <>
-      <div className='bg-violet-200 w-9/12 min-h-[80vh] m-auto p-10 rounded-3xl shadow-xl mb-16'>
-        <div className='text-center text-3xl font-bold text-violet-900 mb-14'>✔ <span className='underline'>To-Do List</span></div>
-        <div className='text-violet-900 text-2xl font-bold mb-5'>Add a To-Do</div>
-        <div className='flex mb-10'>
-          <input type="text" name='task' id='hello' className='w-full p-3 rounded-lg' value={todo.desc} onChange={e => { settodo({ ...todo, desc: e.target.value }) }} />
-          <button onClick={handleAdd} className='bg-violet-800 text-white p-2 rounded-lg ml-14 box-border w-20 hover:bg-violet-700'>Add</button>
+    <div>
+      <div className='bg-violet-200 w-9/12 min-h-[80vh] m-auto p-10 rounded-3xl shadow-xl mb-16 max-md:w-11/12 max-[450px]:p-5'>
+        <div className='text-center text-3xl font-bold text-violet-900 mb-14 max-md:text-2xl'>✔ <span className='underline'>To-Do List</span></div>
+        <div className='text-violet-900 text-2xl font-bold mb-5 max-md:text-xl'>Add a To-Do</div>
+        <div className='flex mb-10 justify-between'>
+          <input type="text" name='task' id='hello' className='w-10/12 p-3 rounded-lg max-md:h-10 max-md:text-sm' value={todo.desc} onChange={e => { settodo({ ...todo, desc: e.target.value }) }} />
+          <button onClick={handleAdd} className='bg-violet-800 text-white p-2 rounded-lg ml-14 box-border w-20 hover:bg-violet-700 max-md:ml-3 max-md:h-10 max-md:flex max-md:items-center max-md:justify-center max-md:text-sm'>Add</button>
         </div>
-        <div className='text-violet-900 text-2xl font-bold mb-5'>Your To-Dos</div>
+        <div className='text-violet-900 text-2xl font-bold mb-5 max-md:text-xl'>Your To-Dos</div>
         <div>
-          <div className='text-violet-900 text-xl font-bold mb-5'>Show Compeleted Tasks <span className='ml-'><input type="checkbox" onChange={handleList} /></span>
+          <div className='text-violet-900 text-xl font-bold mb-5 max-md:text-lg'>Show Compeleted Tasks <span className='ml-'><input type="checkbox" onChange={handleList} /></span>
           </div>
           {todos.map(item => {
             if (showCompleted) {
-              return <div className='bg-violet-100 p-5 rounded-xl flex mb-3' key={uuidv4()}>
+              return <div className='bg-violet-100 p-5 rounded-xl flex mb-3 items-center max-md:p-4' key={uuidv4()}>
                 <input type="checkbox" className='mr-3' checked={item.isCompleted} onChange={() => handleCheck(item.id)} />
-                <span className={`w-full p-2 ${item.isCompleted? 'line-through' : ''}`}>{item.desc}</span>
-                <button onClick={() => handleEdit(item.id)} className={`bg-violet-800 text-white p-2 rounded-lg ml-14 box-border w-20 hover:bg-violet-700 ${item.isCompleted?'hidden':''}`}>Edit</button>
-                <button onClick={() => handleDelete(item.id)} className={`bg-violet-800 text-white p-2 rounded-lg ml-3 box-border w-20 hover:bg-violet-700`}>Delete</button>
+                <span className={`w-full p-2 max-md:whitespace-nowrap max-md:overflow-x-auto max-md:text-sm ${item.isCompleted? 'line-through' : ''}`}>{item.desc}</span>
+                <button onClick={() => handleEdit(item.id)} className={`bg-violet-800 text-white p-2 rounded-lg ml-14 box-border w-20 hover:bg-violet-700 h-10 ${item.isCompleted?'hidden':'flex'} max-md:ml-3 max-md:h-10 max-md:items-center justify-center max-md:text-sm`}>Edit</button>
+                <button onClick={() => handleDelete(item.id)} className={`bg-violet-800 text-white p-2 rounded-lg ml-3 box-border w-20 hover:bg-violet-700 h-10 max-md:h-10 max-md:flex max-md:items-center max-md:justify-center max-md:text-sm max-sm:ml-1`}>Delete</button>
               </div>
             } else {
               if (item.isCompleted === false) {
-                return <div className='bg-violet-100 p-5 rounded-xl flex mb-3' key={uuidv4()}>
+                return <div className='bg-violet-100 p-5 rounded-xl flex mb-3 items-center max-md:p-4' key={uuidv4()}>
                   <input type="checkbox" className='mr-3' onChange={() => handleCheck(item.id)} />
-                  <span className='w-full p-2'>{item.desc}</span>
-                  <button onClick={() => handleEdit(item.id)} className='bg-violet-800 text-white p-2 rounded-lg ml-14 box-border w-20 hover:bg-violet-700'>Edit</button>
-                  <button onClick={() => handleDelete(item.id)} className='bg-violet-800 text-white p-2 rounded-lg ml-3 box-border w-20 hover:bg-violet-700'>Delete</button>
+                  <span className='w-full p-2 max-md:whitespace-nowrap max-md:overflow-x-auto max-md:text-sm'>{item.desc}</span>
+                  <button onClick={() => handleEdit(item.id)} className='bg-violet-800 text-white p-2 rounded-lg ml-14 box-border w-20 hover:bg-violet-700 h-10 max-md:ml-3 max-md:h-10 max-md:flex max-md:items-center max-md:justify-center max-md:text-sm'>Edit</button>
+                  <button onClick={() => handleDelete(item.id)} className='bg-violet-800 text-white p-2 rounded-lg ml-3 box-border w-20 hover:bg-violet-700 h-10 max-md:h-10 max-md:flex max-md:items-center max-md:justify-center max-md:text-sm max-sm:ml-1'>Delete</button>
                 </div>
               }
             }
           })}
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const NavBar = () => {
   return (
     <>
-        <nav className='bg-violet-600 text-white justify-between flex p-3 px-8 mb-10'>
+        <nav className='bg-violet-600 text-white justify-between flex p-3 px-8 mb-10 max-[350px]:px-3'>
             <div className="logo font-bold text-lg p-2">
-                <span>To-Do-List</span>
+                <span className='max-sm:text-base'>To-Do-List</span>
             </div>
-            <ul className='flex text-lg gap-10'>
+            <ul className='flex text-lg gap-3 max-sm:text-base max-[350px]:gap-0'>
                 <li className='cursor-pointer hover:bg-violet-800 p-2 rounded-xl'>Home</li>
                 <li className='cursor-pointer hover:bg-violet-800 p-2 rounded-xl'>Your Tasks</li>
             </ul>
